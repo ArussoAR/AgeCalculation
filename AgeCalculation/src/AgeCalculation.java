76b85e44856da2f799814338d33335316a299d86
alexandrarusso <alexandrarusso@alexandras-mbp>
2020-11-02 19:11:06 -0500

    new ageCalculation

--- AgeCalculation/src/AgeCalculation.java
+++ AgeCalculation/src/AgeCalculation.java
@@ -9,44 +9,43 @@ public class AgeCalculation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
+		 // accepts the birthdate as a string using JOptionPane
 		 String bday = JOptionPane.showInputDialog("Enter your birthdate (MM/DD/YYYY)");
-		 double day = Double.parseDouble(bday);
-	        //throw new NumberFormatException("For input string: \"" + in + "\"");
-	        return;
-	        
+		String[] birthday = bday.split("/");
+				
+		//birthday month
+	        int birthM = Integer.parseInt(birthday[0]);
+	      //birthday day
+	        int birthD = Integer.parseInt(birthday[1]);
+	        // birthday year
+	        int birthY = Integer.parseInt(birthday[2]);
+				
+	   
+	   System.out.println(birthM);
+	   System.out.println(birthD);
+	   System.out.println(birthY);
+	    
+	   // accepts the current date as a string using JOptionPane
 		 String today = JOptionPane.showInputDialog("Enter today's date (MM/DD/YYYY)");
-		 double day2 = Double.parseDouble(today);
-	}
-		 
-		 AgeCalculation myBirthday() {
-			  var str = "Enter your birthdate (MM/DD/YYYY)";
-			  String[] res;
-			res = str.split("MM");
-			  String[] res2;
-			res2 = str.split("DD");
-			  String[] res3;
-			res3 = str.split("YYYY");
-			return null;
-		 }
-		 
-		 
-		AgeCalculation myDate() {
-			var str = "Enter today's date (MM/DD/YYYY)";
-			  String[] res;
-			res = str.split("MM");
-			  String[] res2;
-			res2 = str.split("DD");
-			  String[] res3;
-			res3 = str.split("YYYY");
-			return null;
+		 String[] day2 = today.split("/");
 		
+		 //todays month
+		 int dayM = Integer.parseInt(day2[0]);
+		 //todays date
+		 int dayD = Integer.parseInt(day2[1]);
+		 // todays year
+		 int dayY = Integer.parseInt(day2[2]);
+		 
+			
+		   System.out.println(dayM);
+		   System.out.println(dayD);
+		   System.out.println(dayY);
+		   
+		  System.out.print("My Age is 20 years 11 months ");
+		 
 	
+
  
-		 //JOptionPane.showMessageDialog(null, myBirthday + "Any text you need");
-
-
-		 //System.exit(0)
-		
 	}
-
 }
+
